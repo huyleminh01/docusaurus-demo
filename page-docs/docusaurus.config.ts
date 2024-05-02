e5b7a80dee94de7d3630aaccf9ab8079b1036d2b
Huy Le <leminhhuy.hcmus@gmail.com>
2024-05-02 13:56:34 +0700

    Update code for deployment

--- page-docs/docusaurus.config.ts
+++ page-docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://huyleminh01.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-demo/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'huyleminh01', // Usually your GitHub org/user name.
